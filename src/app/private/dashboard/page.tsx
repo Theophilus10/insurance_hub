fd78014ede3a109433b468e0bf2bfecc289a0c59
JhayStark <jaamen5@gmail.com>
2023-12-10 14:45:48 +0000

    chore:datatable

--- src/app/private/dashboard/page.tsx
+++ src/app/private/dashboard/page.tsx
@@ -1,24 +1,28 @@
-"use client";
+'use client';
 
-import { Alert, AlertDescription, AlertTitle } from "@app/components/ui/alert";
-import useLayoutContext from "@app/context/useLayoutContext";
-import { Terminal } from "lucide-react";
-import React, { useEffect } from "react";
+import DataTable from '@app/components/datatable/datatable';
+import { Alert, AlertDescription, AlertTitle } from '@app/components/ui/alert';
+import useLayoutContext from '@app/context/useLayoutContext';
+import { Terminal } from 'lucide-react';
+import React, { useEffect } from 'react';
+import { columns } from './columns';
+import { data } from './data';
 
-const page = () => {
+const Page = () => {
   const { setPageDetails } = useLayoutContext();
   useEffect(() => {
-    setPageDetails({ title: "", showTitle: false });
+    setPageDetails({ title: '', showTitle: false });
   }, []);
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <Alert className="w-[500px] bg-orange-200">
-        <Terminal className="h-4 w-4" />
+    <div className='flex w-full h-full justify-center items-center'>
+      {/* <Alert className='w-[500px] bg-orange-200'>
+        <Terminal className='h-4 w-4' />
         <AlertTitle>Anticipate!</AlertTitle>
         <AlertDescription>Dashboard Coming soon</AlertDescription>
-      </Alert>
+      </Alert> */}
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };
 
-export default page;
+export default Page;
