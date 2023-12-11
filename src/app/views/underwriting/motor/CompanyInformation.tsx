93650b842c45819a815099808bae16c5dbed1901
Obed Bamfo <obedbamfo5@gmail.com>
2023-12-11 10:57:55 +0000

    Make changes

--- src/app/views/underwriting/motor/CompanyInformation.tsx
+++ src/app/views/underwriting/motor/CompanyInformation.tsx
@@ -1,12 +1,16 @@
 import Form from "@app/components/forms/Form";
 import SelectField from "@app/components/forms/SelectField";
 import InputField from "@app/components/forms/InputField";
+import IconButton from "@app/components/ui/IconButton";
+import { Button } from "@app/components/ui/button";
 import { object, string } from "zod";
+import Divider from "@app/components/ui/Divider";
 
 const options = [
   { value: "Glico General Insurance Company Limited", label: "Glico" },
   { value: "Ghana Union Assurance", label: "GUA" },
 ];
+
 const schema = object({
   username: string(),
   password: string().min(8),
@@ -15,16 +19,67 @@ const schema = object({
 const CompanyInformation = () => {
   return (
     <Form schema={schema}>
-      <SelectField
-        label="Select Company"
-        name="company"
-        required
-        options={options}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 py-5">
+        <SelectField
+          label="Insurance Company"
+          name="company"
+          required
+          options={options}
+        />
+        <SelectField
+          label="Select Branch"
+          name="branch"
+          required
+          options={options}
+        />
+      </div>
+      <Divider />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 py-5">
+        <SelectField
+          label="Distribution Channel"
+          name="distribution_channel"
+          required
+          options={options}
+        />
+        <SelectField
+          label="Intermediary Type"
+          name="intermediary_type"
+          required
+          options={options}
+        />
+        <SelectField
+          label="Intermediary Name"
+          name="intermediary_name"
+          required
+          options={options}
+        />
+        <SelectField
+          label="Branch Office of Intermediary"
+          name="intermediary_branch_ofice"
+          required
+          options={options}
+        />
+      </div>
+      <Divider />
+      <InputField label="fullname or ID or Email " name="find_customer" />
+      <div className="flex py-5 gap-6 ">
+        <IconButton icon="mdi:search" color="primary" />
+        <Button>Add New Customer</Button>
+      </div>
+
+      <InputField
+        label="First Name"
+        name="first_name"
+        value="yes"
+        disabled={true}
       />
-      <InputField label="code" name="code" disabled={true} />
+
+      {/* Add responsive styling to the other fields as needed */}
+      {/* <InputField label="code" name="code" disabled={true} />
       <InputField label="Phone" name="phone" disabled={true} />
       <InputField label="Email" name="email" disabled={true} />
-      <InputField label="Address" name="adress" disabled={true} />
+      <InputField label="Address" name="address" disabled={true} /> */}
     </Form>
   );
 };
--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,117 +1,212 @@
-import * as React from 'react';
+"use client";
 
-import { cn } from '@app/lib/utils';
+import React, { useReducer } from "react";
+import DataTable, {
+  TableColumn,
+  createTheme,
+} from "react-data-table-component";
+import TableSearchbox from "./tableSearchbox";
+import { Button } from "./button";
+import SideModal from "./sideModal";
+import { ContentSize } from "@app/types/appTypes";
+import AlertModal from "../alerts/alertModal";
+import TableFilter from "./tableFilter";
+import Modal from "./modal";
 
-const Table = React.forwardRef<
-  HTMLTableElement,
-  React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
-  <div className='relative w-full overflow-auto'>
-    <table
-      ref={ref}
-      className={cn('w-full caption-bottom text-sm', className)}
-      {...props}
-    />
-  </div>
-));
-Table.displayName = 'Table';
+const data = [
+  {
+    id: 1,
+    name: "Osei Adu",
+    customer_type: "Individual",
+    identification_type: "National ID",
+    idNumber: "GHA-712344944-3",
+    customer_category: "Citizen",
+    occupation: "Self Employed",
+  },
+  {
+    id: 2,
+    name: "John Adu",
+    customer_type: "Individual",
+    identification_type: "National ID",
+    idNumber: "GHA-7123242444-3",
+    customer_category: "Citizen",
+    occupation: "Public Servant",
+  },
+  {
+    id: 3,
+    name: "Alex Blay",
+    customer_type: "Individual",
+    identification_type: "National ID",
+    idNumber: "GHA-871344944-3",
+    customer_category: "Citizen",
+    occupation: "Self Employed",
+  },
+  {
+    id: 4,
+    name: "Osei Adu",
+    customer_type: "Individual",
+    identification_type: "National ID",
+    idNumber: "GHA-712344944-3",
+    customer_category: "Citizen",
+    occupation: "Self Employed",
+  },
+  {
+    id: 5,
+    name: "Osei Adu",
+    customer_type: "Individual",
+    identification_type: "National ID",
+    idNumber: "GHA-712344944-3",
+    customer_category: "Citizen",
+    occupation: "Self Employed",
+  },
+];
 
-const TableHeader = React.forwardRef<
-  HTMLTableSectionElement,
-  React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn('[&_tr]:border-b', className)} {...props} />
-));
-TableHeader.displayName = 'TableHeader';
+export type DataRow = Record<string, any>;
 
-const TableBody = React.forwardRef<
-  HTMLTableSectionElement,
-  React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <tbody
-    ref={ref}
-    className={cn('[&_tr:last-child]:border-0', className)}
-    {...props}
-  />
-));
-TableBody.displayName = 'TableBody';
+interface ITable {
+  addNewRecordLabel?: string;
+  addButtonLabel?: string;
+  updateRecordLabel?: string;
+  modalSize?: ContentSize;
+  Editor?: any;
+  searchPlaceholder?: string;
+  showAddButton?: boolean;
+  showSearchBox?: boolean;
+  tableColumn: TableColumn<DataRow>[];
+}
 
-const TableFooter = React.forwardRef<
-  HTMLTableSectionElement,
-  React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <tfoot
-    ref={ref}
-    className={cn(
-      'border-t bg-slate-100/50 font-medium [&>tr]:last:border-b-0 dark:bg-slate-800/50',
-      className
-    )}
-    {...props}
-  />
-));
-TableFooter.displayName = 'TableFooter';
+const Table: React.FC<ITable> = ({
+  addButtonLabel = "New Record",
+  addNewRecordLabel = "Add New Record",
+  updateRecordLabel = "Update Record",
+  modalSize = "md",
+  Editor,
+  searchPlaceholder = "Search...",
+  showAddButton = true,
+  showSearchBox = true,
+  tableColumn,
+}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const TableRow = React.forwardRef<
-  HTMLTableRowElement,
-  React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
-  <tr
-    ref={ref}
-    className={cn(
-      'border-b transition-colors hover:bg-slate-100/50 data-[state=selected]:bg-slate-100 dark:hover:bg-slate-800/50 dark:data-[state=selected]:bg-slate-800',
-      className
-    )}
-    {...props}
-  />
-));
-TableRow.displayName = 'TableRow';
+  const toggleSideModal = () => {
+    if (!state.openSideModal)
+      dispatch({ type: "SET_EDITOR_HEADING", payload: addNewRecordLabel });
+    dispatch({ type: "TOGGLE_SIDE_MODAL", payload: !state.openSideModal });
+  };
 
-const TableHead = React.forwardRef<
-  HTMLTableCellElement,
-  React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
-  <th
-    ref={ref}
-    className={cn(
-      'h-12 px-4 text-left align-middle font-medium text-slate-500 [&:has([role=checkbox])]:pr-0 dark:text-slate-400',
-      className
-    )}
-    {...props}
-  />
-));
-TableHead.displayName = 'TableHead';
+  const toggleAlertModal = () => {
+    dispatch({ type: "TOGGLE_ALERT_MODAL", payload: !state.openAlertModal });
+  };
 
-const TableCell = React.forwardRef<
-  HTMLTableCellElement,
-  React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
-  <td
-    ref={ref}
-    className={cn('p-4 align-middle [&:has([role=checkbox])]:pr-0', className)}
-    {...props}
-  />
-));
-TableCell.displayName = 'TableCell';
+  const handleAlertMutation = () => {
+    dispatch({ type: "TOGGLE_LOAD_DELETE", payload: !state.loadDelete });
+    setTimeout(() => {
+      dispatch({ type: "TOGGLE_ALERT_MODAL", payload: !state.openAlertModal });
+      dispatch({ type: "TOGGLE_LOAD_DELETE", payload: !state.loadDelete });
+    }, 700);
+  };
 
-const TableCaption = React.forwardRef<
-  HTMLTableCaptionElement,
-  React.HTMLAttributes<HTMLTableCaptionElement>
->(({ className, ...props }, ref) => (
-  <caption
-    ref={ref}
-    className={cn('mt-4 text-sm text-slate-500 dark:text-slate-400', className)}
-    {...props}
-  />
-));
-TableCaption.displayName = 'TableCaption';
+  return (
+    <div className="w-full h-full flex flex-col">
+      <div className="flex justify-between">
+        <div className={`flex-grow max-w-md ${!showSearchBox && "hidden"}`}>
+          <TableSearchbox placeholder={searchPlaceholder} />
+        </div>
+        <div className="flex items-center gap-4">
+          <div>
+            <TableFilter />
+          </div>
+          <div className={`${!showAddButton && "hidden"}`}>
+            <Button
+              variant="primary"
+              label={addButtonLabel}
+              onClick={toggleSideModal}
+            />
+          </div>
+        </div>
+      </div>
+      <DataTable
+        columns={tableColumn}
+        data={data}
+        striped
+        pagination
+        responsive
+        defaultSortFieldId={1}
+        defaultSortAsc
 
-export {
-  Table,
-  TableHeader,
-  TableBody,
-  TableFooter,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableCaption,
+        // theme="solarized"
+      />
+      <Modal
+        open={state.openSideModal}
+        size={modalSize}
+        title={state.editorHeading}
+        closeModal={toggleSideModal}
+      >
+        <Editor />
+      </Modal>
+      {/* <SideModal
+        open={state.openSideModal}
+        closeModal={toggleSideModal}
+        size={modalSize}
+       
+      >
+        <Editor />
+      </SideModal> */}
+      <AlertModal
+        open={state.openAlertModal}
+        onCancel={toggleAlertModal}
+        onContinue={handleAlertMutation}
+        busy={state.loadDelete}
+      />
+    </div>
+  );
+};
+
+export default Table;
+
+const initialState = {
+  openSideModal: false,
+  addRecord: false,
+  updateRecord: false,
+  editorHeading: "",
+  openAlertModal: false,
+  loadDelete: false,
+};
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "TOGGLE_SIDE_MODAL":
+      return {
+        ...state,
+        openSideModal: action.payload,
+      };
+    case "TOGGLE_ADD_RECORD":
+      return {
+        ...state,
+        addRcord: action.payload,
+      };
+    case "TOGGLE_UPDATE_RECORD":
+      return {
+        ...state,
+        updateRecord: action.payload,
+      };
+    case "TOGGLE_LOAD_DELETE":
+      return {
+        ...state,
+        loadDelete: action.payload,
+      };
+    case "TOGGLE_ALERT_MODAL":
+      return {
+        ...state,
+        openAlertModal: action.payload,
+      };
+    case "SET_EDITOR_HEADING":
+      return {
+        ...state,
+        editorHeading: action.payload,
+      };
+
+    default:
+      return state;
+  }
 };
