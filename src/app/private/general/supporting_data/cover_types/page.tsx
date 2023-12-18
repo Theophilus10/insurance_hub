f4f61e80127aa79b5539d7e87a3fc53677e4ede7
littAmui <samuel.amui@leanicontechnology.com>
2023-12-18 10:08:00 +0000

    create the policy extensions page

--- src/app/private/general/supporting_data/cover_types/page.tsx
+++ src/app/private/general/supporting_data/cover_types/page.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import React, { useState } from "react";
+import Editor from "./partials/editor";
+import Modal from "@app/components/ui/modal";
+import DataTable from "@app/components/datatable/datatable";
+import { data } from "./partials/data";
+import { columns } from "./partials/columns";
+
+const Page = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [editorLabel, setEditorLabel] = useState("Add Policy Extension");
+  const [preValues,setPreValues] = useState({})
+  const toggleModal = () => {
+    setEditorLabel("Add Policy Extension");
+    setPreValues({})
+    setOpenModal(!openModal);
+  };
+
+  const onRowAction = (action: string, row: Record<string, any>) => {
+    switch (action) {
+      case "edit":
+        setEditorLabel("Update Policy Extension");
+        setOpenModal(true);
+        setPreValues(row)
+        break;
+      case "delete":
+        alert("delete");
+
+        break;
+
+      default:
+        break;
+    }
+  };
   return (
-    <div>
-      Cover types
+    <div className="flex flex-col w-full h-full gap-4 pt-2">
+      <div className={` text-gray-500 font-medium text-[22px]`}>
+        Policy Extension List
+      </div>
+      <DataTable
+        data={data}
+        columns={columns}
+        addButtonLabel="Add Policy Extension"
+        addButtonFunction={toggleModal}
+        showActions
+        onRowAction={onRowAction}
+      />
+      <Modal
+        open={openModal}
+        size="md"
+        title={editorLabel}
+        closeModal={toggleModal}
+      >
+        <Editor prevalues={preValues}/>
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
